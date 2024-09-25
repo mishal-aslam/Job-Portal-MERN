3ed78be52c1b22e862d3e2dd396e3f4fc3e8e36c
Mishal Aslam <mishalaslam17@gmail.com>
2024-09-25 20:38:06 +0500

    start part2

---
+++ job-finder-client/src/Pages/CreateJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateJob = () => {
+  return (
+    <div className='max-w-screen-2xl conatiner mx-auto xl:px-24 px-4'>
+      CreateJob
+    </div>
+  )
+}
+
+export default CreateJob
--- job-finder-client/src/Router/Router.jsx
+++ job-finder-client/src/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import CreateJob from "../Pages/CreateJob";
 
 const router = createBrowserRouter([
   {
@@ -9,7 +10,11 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
         { path: "/", element: <Home /> },
-        { path: "/about", element: <About /> },
+        // { path: "/about", element: <About /> },
+        {
+          path: "/Post-job",
+          element: <CreateJob />
+        }
     ],
   },
 ]);
